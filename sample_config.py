1b01e7bf5ec6afbdb575dc6ad41db83e46e06576
Two? <90793941+twodrk@users.noreply.github.com>
2022-01-24 11:21:38 +0400

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -13,8 +13,8 @@ if not HEROKU:
     bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
     ARQ_API_KEY = "Get this from @ARQRobot"
 # List of supported languages >>
-# https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
-    LANGUAGE = "en"
+# https://py-googletrans.readthedocs.io/tr/latest/#googletrans-languages
+    LANGUAGE = "tr"
 
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
